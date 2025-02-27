8d93ac4218e7b371aacf10b9ecf5b25c81be2e10
Herve Rousseau <herve.rousseau@cern.ch>
2025-02-27 09:06:43 +0100

    Broken logic fix

--- src/core/DataManager.cpp
+++ src/core/DataManager.cpp
@@ -365,195 +365,191 @@ void DataManager::queueFlightplanUpdate(EuroScopePlugIn::CFlightPlan flightplan)
     // skip the update if:
     // - the flightplan or its data is invalid
     // - or the aircraft is out of range therefore GetSimulated() is true
-    //  - Above 5000ft
     //  - More than 10nm away from origin
     if (false == flightplan.IsValid() || nullptr == flightplan.GetFlightPlanData().GetPlanType() ||
-        nullptr == flightplan.GetFlightPlanData().GetOrigin() || flightlan.isSimulated() ||
-        flightplan.trueAltitude > 5000 || flightplan.distanceFromOrigin > 10.0) {
+        nullptr == flightplan.GetFlightPlanData().GetOrigin() || flightlan.GetSimulated() ||
+        flightplan.GetDistanceFromOrigin() > 10.0) {
         return;
-
-        auto pilot = this->CFlightPlanToPilot(flightplan);
-
-        std::lock_guard guard(this->m_euroscopeUpdatesLock);
-        this->m_euroscopeFlightplanUpdates.push_back({std::chrono::utc_clock::now(), pilot});
     }
 
-    void DataManager::consolidateWithBackend(std::map<std::string, std::array<types::Pilot, 3U>> & pilots) {
-        // retrieving backend data
-        auto backendPilots = Server::instance().getPilots(this->m_activeAirports);
+    auto pilot = this->CFlightPlanToPilot(flightplan);
 
-        for (auto pilot = pilots.begin(); pilots.end() != pilot;) {
-            // update backend data & consolidate
-            bool removeFlight = pilot->second[ServerData].inactive == true;
-            for (auto updateIt = backendPilots.begin(); updateIt != backendPilots.end(); ++updateIt) {
-                if (updateIt->callsign == pilot->second[EuroscopeData].callsign) {
-                    Logger::instance().log(
-                        Logger::LogSender::DataManager,
-                        "Updating " + pilot->second[EuroscopeData].callsign + " with" + updateIt->callsign,
-                        Logger::LogLevel::Info);
-                    pilot->second[ServerData] = *updateIt;
-                    DataManager::consolidateData(pilot->second);
-                    removeFlight = false;
-                    updateIt = backendPilots.erase(updateIt);
-                    break;
-                }
-            }
+    std::lock_guard guard(this->m_euroscopeUpdatesLock);
+    this->m_euroscopeFlightplanUpdates.push_back({std::chrono::utc_clock::now(), pilot});
+}
 
-            // remove pilot if he has been flagged as inactive from the backend
-            if (true == removeFlight) {
-                pilot = pilots.erase(pilot);
-            } else {
-                ++pilot;
+void DataManager::consolidateWithBackend(std::map<std::string, std::array<types::Pilot, 3U>>& pilots) {
+    // retrieving backend data
+    auto backendPilots = Server::instance().getPilots(this->m_activeAirports);
+
+    for (auto pilot = pilots.begin(); pilots.end() != pilot;) {
+        // update backend data & consolidate
+        bool removeFlight = pilot->second[ServerData].inactive == true;
+        for (auto updateIt = backendPilots.begin(); updateIt != backendPilots.end(); ++updateIt) {
+            if (updateIt->callsign == pilot->second[EuroscopeData].callsign) {
+                Logger::instance().log(
+                    Logger::LogSender::DataManager,
+                    "Updating " + pilot->second[EuroscopeData].callsign + " with" + updateIt->callsign,
+                    Logger::LogLevel::Info);
+                pilot->second[ServerData] = *updateIt;
+                DataManager::consolidateData(pilot->second);
+                removeFlight = false;
+                updateIt = backendPilots.erase(updateIt);
+                break;
             }
         }
-    }
 
-    void DataManager::consolidateData(std::array<types::Pilot, 3> & pilot) {
-        if (pilot[EuroscopeData].callsign == pilot[ServerData].callsign) {
-            // backend data
-            pilot[ConsolidatedData].inactive = pilot[ServerData].inactive;
-            pilot[ConsolidatedData].lastUpdate = pilot[ServerData].lastUpdate;
-
-            pilot[ConsolidatedData].eobt = pilot[ServerData].eobt;
-            pilot[ConsolidatedData].tobt = pilot[ServerData].tobt;
-            pilot[ConsolidatedData].tobt_state = pilot[ServerData].tobt_state;
-            pilot[ConsolidatedData].ctot = pilot[ServerData].ctot;
-            pilot[ConsolidatedData].ttot = pilot[ServerData].ttot;
-            pilot[ConsolidatedData].tsat = pilot[ServerData].tsat;
-            pilot[ConsolidatedData].exot = pilot[ServerData].exot;
-            pilot[ConsolidatedData].asat = pilot[ServerData].asat;
-            pilot[ConsolidatedData].aobt = pilot[ServerData].aobt;
-            pilot[ConsolidatedData].atot = pilot[ServerData].atot;
-            pilot[ConsolidatedData].asrt = pilot[ServerData].asrt;
-            pilot[ConsolidatedData].aort = pilot[ServerData].aort;
-
-            pilot[ConsolidatedData].measures = pilot[ServerData].measures;
-            pilot[ConsolidatedData].hasBooking = pilot[ServerData].hasBooking;
-            pilot[ConsolidatedData].taxizoneIsTaxiout = pilot[ServerData].taxizoneIsTaxiout;
-
-            // EuroScope data
-            pilot[ConsolidatedData].latitude = pilot[EuroscopeData].latitude;
-            pilot[ConsolidatedData].longitude = pilot[EuroscopeData].longitude;
-
-            pilot[ConsolidatedData].origin = pilot[EuroscopeData].origin;
-            pilot[ConsolidatedData].destination = pilot[EuroscopeData].destination;
-            pilot[ConsolidatedData].runway = pilot[EuroscopeData].runway;
-            pilot[ConsolidatedData].sid = pilot[EuroscopeData].sid;
-
-            logging::Logger::instance().log(Logger::LogSender::DataManager,
-                                            "Consolidated " + pilot[ServerData].callsign,
-                                            logging::Logger::LogLevel::Info);
+        // remove pilot if he has been flagged as inactive from the backend
+        if (true == removeFlight) {
+            pilot = pilots.erase(pilot);
         } else {
-            logging::Logger::instance().log(Logger::LogSender::DataManager,
-                                            "Callsign mismatch during consolidation: " + pilot[EuroscopeData].callsign +
-                                                ", " + pilot[ServerData].callsign,
-                                            logging::Logger::LogLevel::Critical);
+            ++pilot;
         }
     }
+}
 
-    void DataManager::processEuroScopeUpdates(std::map<std::string, std::array<types::Pilot, 3U>> & pilots) {
-        // obtain a copy of the flightplan updates, clear the update list, consolidate flightplan updates
-        this->m_euroscopeUpdatesLock.lock();
-        auto flightplanUpdates = this->m_euroscopeFlightplanUpdates;
-        this->m_euroscopeFlightplanUpdates.clear();
-        this->m_euroscopeUpdatesLock.unlock();
+void DataManager::consolidateData(std::array<types::Pilot, 3>& pilot) {
+    if (pilot[EuroscopeData].callsign == pilot[ServerData].callsign) {
+        // backend data
+        pilot[ConsolidatedData].inactive = pilot[ServerData].inactive;
+        pilot[ConsolidatedData].lastUpdate = pilot[ServerData].lastUpdate;
 
-        this->consolidateFlightplanUpdates(flightplanUpdates);
+        pilot[ConsolidatedData].eobt = pilot[ServerData].eobt;
+        pilot[ConsolidatedData].tobt = pilot[ServerData].tobt;
+        pilot[ConsolidatedData].tobt_state = pilot[ServerData].tobt_state;
+        pilot[ConsolidatedData].ctot = pilot[ServerData].ctot;
+        pilot[ConsolidatedData].ttot = pilot[ServerData].ttot;
+        pilot[ConsolidatedData].tsat = pilot[ServerData].tsat;
+        pilot[ConsolidatedData].exot = pilot[ServerData].exot;
+        pilot[ConsolidatedData].asat = pilot[ServerData].asat;
+        pilot[ConsolidatedData].aobt = pilot[ServerData].aobt;
+        pilot[ConsolidatedData].atot = pilot[ServerData].atot;
+        pilot[ConsolidatedData].asrt = pilot[ServerData].asrt;
+        pilot[ConsolidatedData].aort = pilot[ServerData].aort;
 
-        for (auto& update : flightplanUpdates) {
-            bool found = false;
+        pilot[ConsolidatedData].measures = pilot[ServerData].measures;
+        pilot[ConsolidatedData].hasBooking = pilot[ServerData].hasBooking;
+        pilot[ConsolidatedData].taxizoneIsTaxiout = pilot[ServerData].taxizoneIsTaxiout;
 
-            const auto pilot = update.data;
+        // EuroScope data
+        pilot[ConsolidatedData].latitude = pilot[EuroscopeData].latitude;
+        pilot[ConsolidatedData].longitude = pilot[EuroscopeData].longitude;
 
-            // find pilot in list
-            for (auto& pair : pilots) {
-                if (pilot.callsign == pair.first) {
-                    Logger::instance().log(Logger::LogSender::DataManager, "Updated data of " + pilot.callsign,
-                                           Logger::LogLevel::Info);
+        pilot[ConsolidatedData].origin = pilot[EuroscopeData].origin;
+        pilot[ConsolidatedData].destination = pilot[EuroscopeData].destination;
+        pilot[ConsolidatedData].runway = pilot[EuroscopeData].runway;
+        pilot[ConsolidatedData].sid = pilot[EuroscopeData].sid;
 
-                    pair.second[EuroscopeData] = pilot;
-                    found = true;
-                    break;
-                }
-            }
+        logging::Logger::instance().log(Logger::LogSender::DataManager, "Consolidated " + pilot[ServerData].callsign,
+                                        logging::Logger::LogLevel::Info);
+    } else {
+        logging::Logger::instance().log(Logger::LogSender::DataManager,
+                                        "Callsign mismatch during consolidation: " + pilot[EuroscopeData].callsign +
+                                            ", " + pilot[ServerData].callsign,
+                                        logging::Logger::LogLevel::Critical);
+    }
+}
 
-            if (false == found) {
-                Logger::instance().log(Logger::LogSender::DataManager, "Added " + pilot.callsign,
+void DataManager::processEuroScopeUpdates(std::map<std::string, std::array<types::Pilot, 3U>>& pilots) {
+    // obtain a copy of the flightplan updates, clear the update list, consolidate flightplan updates
+    this->m_euroscopeUpdatesLock.lock();
+    auto flightplanUpdates = this->m_euroscopeFlightplanUpdates;
+    this->m_euroscopeFlightplanUpdates.clear();
+    this->m_euroscopeUpdatesLock.unlock();
+
+    this->consolidateFlightplanUpdates(flightplanUpdates);
+
+    for (auto& update : flightplanUpdates) {
+        bool found = false;
+
+        const auto pilot = update.data;
+
+        // find pilot in list
+        for (auto& pair : pilots) {
+            if (pilot.callsign == pair.first) {
+                Logger::instance().log(Logger::LogSender::DataManager, "Updated data of " + pilot.callsign,
                                        Logger::LogLevel::Info);
-                pilots.insert({pilot.callsign, {pilot, pilot, types::Pilot()}});
+
+                pair.second[EuroscopeData] = pilot;
+                found = true;
+                break;
             }
         }
+
+        if (false == found) {
+            Logger::instance().log(Logger::LogSender::DataManager, "Added " + pilot.callsign, Logger::LogLevel::Info);
+            pilots.insert({pilot.callsign, {pilot, pilot, types::Pilot()}});
+        }
     }
+}
 
-    void DataManager::consolidateFlightplanUpdates(std::list<EuroscopeFlightplanUpdate> & inputList) {
-        std::list<DataManager::EuroscopeFlightplanUpdate> resultList;
+void DataManager::consolidateFlightplanUpdates(std::list<EuroscopeFlightplanUpdate>& inputList) {
+    std::list<DataManager::EuroscopeFlightplanUpdate> resultList;
 
-        for (const auto& currentUpdate : inputList) {
-            auto pilot = currentUpdate.data;
+    for (const auto& currentUpdate : inputList) {
+        auto pilot = currentUpdate.data;
 
-            // only handle updates for active airports
-            {
-                std::lock_guard guard(this->m_airportLock);
-                bool flightDepartsFromActiveAirport = std::find(m_activeAirports.begin(), m_activeAirports.end(),
-                                                                std::string(pilot.origin)) != m_activeAirports.end();
-                if (false == flightDepartsFromActiveAirport) continue;
-            }
+        // only handle updates for active airports
+        {
+            std::lock_guard guard(this->m_airportLock);
+            bool flightDepartsFromActiveAirport = std::find(m_activeAirports.begin(), m_activeAirports.end(),
+                                                            std::string(pilot.origin)) != m_activeAirports.end();
+            if (false == flightDepartsFromActiveAirport) continue;
+        }
 
-            // Check if the flight plan already exists in the result list
-            auto it = std::find_if(resultList.begin(), resultList.end(),
-                                   [&currentUpdate](const EuroscopeFlightplanUpdate& existingUpdate) {
-                                       return existingUpdate.data.callsign == currentUpdate.data.callsign;
-                                   });
+        // Check if the flight plan already exists in the result list
+        auto it = std::find_if(resultList.begin(), resultList.end(),
+                               [&currentUpdate](const EuroscopeFlightplanUpdate& existingUpdate) {
+                                   return existingUpdate.data.callsign == currentUpdate.data.callsign;
+                               });
 
-            if (it != resultList.end()) {
-                // Flight plan with the same callsign exists
-                // Check if the timeIssued is newer
-                if (currentUpdate.timeIssued > it->timeIssued) {
-                    // Update with the newer data
-                    *it = currentUpdate;
-                    Logger::instance().log(Logger::LogSender::DataManager,
-                                           "Updated: " + std::string(currentUpdate.data.callsign),
-                                           Logger::LogLevel::Info);
-                } else {
-                    // Existing data is already newer, no update needed
-                    Logger::instance().log(Logger::LogSender::DataManager,
-                                           "Skipped old update for: " + std::string(currentUpdate.data.callsign),
-                                           Logger::LogLevel::Info);
-                }
-            } else {
-                // Flight plan with the callsign doesn't exist, add it to the result list
-                resultList.push_back(currentUpdate);
+        if (it != resultList.end()) {
+            // Flight plan with the same callsign exists
+            // Check if the timeIssued is newer
+            if (currentUpdate.timeIssued > it->timeIssued) {
+                // Update with the newer data
+                *it = currentUpdate;
                 Logger::instance().log(Logger::LogSender::DataManager,
-                                       "Update added: " + std::string(currentUpdate.data.callsign),
+                                       "Updated: " + std::string(currentUpdate.data.callsign), Logger::LogLevel::Info);
+            } else {
+                // Existing data is already newer, no update needed
+                Logger::instance().log(Logger::LogSender::DataManager,
+                                       "Skipped old update for: " + std::string(currentUpdate.data.callsign),
                                        Logger::LogLevel::Info);
             }
+        } else {
+            // Flight plan with the callsign doesn't exist, add it to the result list
+            resultList.push_back(currentUpdate);
+            Logger::instance().log(Logger::LogSender::DataManager,
+                                   "Update added: " + std::string(currentUpdate.data.callsign), Logger::LogLevel::Info);
         }
-
-        inputList = resultList;
     }
 
-    types::Pilot DataManager::CFlightPlanToPilot(const EuroScopePlugIn::CFlightPlan flightplan) {
-        types::Pilot pilot;
+    inputList = resultList;
+}
 
-        pilot.callsign = flightplan.GetCallsign();
-        pilot.lastUpdate = std::chrono::utc_clock::now();
+types::Pilot DataManager::CFlightPlanToPilot(const EuroScopePlugIn::CFlightPlan flightplan) {
+    types::Pilot pilot;
 
-        // position data
-        pilot.latitude = flightplan.GetFPTrackPosition().GetPosition().m_Latitude;
-        pilot.longitude = flightplan.GetFPTrackPosition().GetPosition().m_Longitude;
-        pilot.trueAltitude = flightplan.GetFPTrackPosition().GetPressureAltitude();
-        pilot.distanceFromOrigin = flightplan.GetDistanceFromOrigin();
-        pilot.isSimulated = flightplan.GetSimulated();
+    pilot.callsign = flightplan.GetCallsign();
+    pilot.lastUpdate = std::chrono::utc_clock::now();
 
-        // flightplan & clearance data
-        pilot.origin = flightplan.GetFlightPlanData().GetOrigin();
-        pilot.destination = flightplan.GetFlightPlanData().GetDestination();
-        pilot.runway = flightplan.GetFlightPlanData().GetDepartureRwy();
-        pilot.sid = flightplan.GetFlightPlanData().GetSidName();
+    // position data
+    pilot.latitude = flightplan.GetFPTrackPosition().GetPosition().m_Latitude;
+    pilot.longitude = flightplan.GetFPTrackPosition().GetPosition().m_Longitude;
+    pilot.trueAltitude = flightplan.GetFPTrackPosition().GetPressureAltitude();
+    pilot.distanceFromOrigin = flightplan.GetDistanceFromOrigin();
+    pilot.isSimulated = flightplan.GetSimulated();
 
-        // acdm data
-        pilot.eobt = utils::Date::convertEuroscopeDepartureTime(flightplan);
-        pilot.tobt = pilot.eobt;
+    // flightplan & clearance data
+    pilot.origin = flightplan.GetFlightPlanData().GetOrigin();
+    pilot.destination = flightplan.GetFlightPlanData().GetDestination();
+    pilot.runway = flightplan.GetFlightPlanData().GetDepartureRwy();
+    pilot.sid = flightplan.GetFlightPlanData().GetSidName();
 
-        return pilot;
-    }
+    // acdm data
+    pilot.eobt = utils::Date::convertEuroscopeDepartureTime(flightplan);
+    pilot.tobt = pilot.eobt;
+
+    return pilot;
+}
